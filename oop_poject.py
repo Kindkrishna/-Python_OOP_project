ce174bb57f8aeb4c95c04c9353476fa186a13947
Kishor Kumar Krishna <kishorkrishna123@gmail.com>
2026-06-14 12:30:10 +0530

    Add files via upload

---
+++ oop_poject.py
@@ -0,0 +1,11 @@
+# (A) : Create a backend bill system for building in a coffee shop:
+'''
+    1. Take order from user and calculate their price
+    2. Keep updateing the total price for all product they ordered
+    3. Generate bill
+    4. Should work for multiple customers simultaneously
+        - Alex = Coffee + Cookie
+        - Bob  = Cookie
+        Alex made a second order for coffee --> Update alex bill with recent coffee
+    5. Try solving this problem without OOPS (simple def functions) and with OOPS
+'''
